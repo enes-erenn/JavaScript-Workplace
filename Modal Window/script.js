6f0b24a2b9e30a38dbc3ec5faf7e5ce81140c55b
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-11-14 15:53:03 +0300

    Modal Window

---
+++ Modal Window/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Declaring variables
+const modal = document.querySelector(".modal");
+const btnsOpen = document.querySelectorAll(".show-modal");
+const btnClose = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+//Declaring functions
+const openModal = function() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = function() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+// Selecting all of the buttons
+for (let i = 0; i < btnsOpen.length; i++) {
+    btnsOpen[i].addEventListener("click", openModal);
+}
+
+// Adding eventlistener to the variables
+btnClose.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal)
+
+// Key pressing to close
+document.addEventListener("keydown", function(e){
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal()
+    }
+}) 

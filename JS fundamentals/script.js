0aebf325c1f1a0beb1de07477fc4ff1bc3534fe9
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-12-05 21:03:22 +0300

    Arrays - Challenge 4

--- JS fundamentals/script.js
+++ JS fundamentals/script.js
@@ -442,3 +442,33 @@ ages
 const avg2 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3])
 console.log(avg2)
 
+// DATA
+const dogs = 
+[
+    { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+    { weight: 8, curFood: 200, owners: ['Matilda'] },
+    { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+    { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => (dog.recFood = Math.floor(dog.weight ** 0.75 * 28)));
+
+const dogSarah = dogs.find(dog => dog.owners.includes("Sarah"));
+
+const ownersEatTooMuch = dogs
+.filter(dog => dog.curFood > dog.recFood)
+.flatMap(dog => dog.owners)
+console.log(ownersEatTooMuch)
+
+const ownersEatTooLittle = dogs
+.filter(dog => dog.curFood < dog.recFood)
+.flatMap(dog => dog.owners)
+console.log(ownersEatTooMuch)
+
+console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too much!'`);
+console.log(`${ownersEatTooLittle.join(" and ")}'s dogs eat too little!'`);
+
+const checkEatingOkay = dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);

381123048d5495bea47aa9ff3ea66dc95be915b9
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-12-08 16:37:28 +0300

    Added Hover Logic to the Nav Links

--- Bankist/script.js
+++ Bankist/script.js
@@ -11,6 +11,10 @@ const header = document.querySelector('.header');
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section = document.querySelector("#section--1");
 const navLinks = document.querySelector(".nav__links");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav")
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -60,3 +64,29 @@ navLinks.addEventListener("click", function (e){
     document.querySelector(id).scrollIntoView( {behavior: "smooth"} );
   }
 });
+
+tabsContainer.addEventListener("click", function(e){
+  e.preventDefault();
+  const clicked = e.target.closest(".operations__tab");
+
+  if(!clicked) return
+    tabs.forEach(t => t.classList.remove("operations__tab--active"))
+    tabsContent.forEach(c => c.classList.remove("operations__content--active"))
+    clicked.classList.add("operations__tab--active")
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active")
+});
+const handleHover = function(e) {
+    if(e.target.classList.contains("nav__link")){
+      const link = e.target;
+      const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+      const logo = link.closest(".nav").querySelector("img");
+      siblings.forEach(el => {
+        if(el !== link) el.style.opacity = this;
+      });
+      logo.style.opacity = this;
+    }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));

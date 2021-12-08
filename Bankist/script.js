96bef85c45fc1f4d6e89a8df1ea490499640e6ac
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-12-08 10:30:46 +0300

    Cookies Button Added and Added Smooth Scrolling to the Learn More Button

--- Bankist/script.js
+++ Bankist/script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section = document.querySelector("#section--1");
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -28,4 +31,23 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const message = document.createElement("div");
+message.classList.add('cookie-message');
+
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+header.prepend(message);
+
+document.querySelector(".btn--close-cookie").addEventListener("click", function(e) {
+  e.preventDefault();
+  message.remove()
+})
+
+message.style.background = "#37383d";
+message.style.width = "120%";
+message.style.height = "5em";
+
+btnScrollTo.addEventListener("click", function(e) {
+  section.scrollIntoView({behavior: "smooth"})
 });

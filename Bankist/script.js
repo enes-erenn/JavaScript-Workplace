d5a5c954b26fd9d0d172bee41eae0f5caee09464
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-12-08 21:13:31 +0300

    Loaded the Images and Improved Loading Performance

--- Bankist/script.js
+++ Bankist/script.js
@@ -16,6 +16,7 @@ const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 const nav = document.querySelector(".nav")
 const allSections = document.querySelectorAll(".section");
+const imgTargets = document.querySelectorAll('img[data-src]');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -110,16 +111,41 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 const revealSection = function(entries, observer){
-const [entry] = entries;
-if(!entry.isIntersecting) return;
-entry.target.classList.remove("section--hidden");
-observer.unobserve(entry.target)
-}
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target)
+};
+
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
-})
+});
+
 allSections.forEach(function(section){
   sectionObserver.observe(section)
   section.classList.add("section--hidden")
-})
+});
+
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));

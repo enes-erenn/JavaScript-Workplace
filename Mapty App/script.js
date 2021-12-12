7b619e7331d5e72b04222972c6f5815f2816534b
Enes Eren <88148105+thenesern@users.noreply.github.com>
2021-12-12 20:31:06 +0300

    Move to Marker On Click

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -13,7 +13,7 @@ let map, mapEvent;
 class Workout {
     date = new Date();
     id = (new Date() + "").slice(-10);
-
+    clicks = 0;
     constructor(coords, distance, duration) {
         this.coords = coords;
         this.distance = distance;
@@ -25,6 +25,10 @@ class Workout {
         "June", "July", "August", "September", "October", "November", "December"];
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on 
         ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    };
+
+    click(){
+        this.click++;
     }
 }
 
@@ -66,6 +70,7 @@ console.log(running1, cycling1); */
 
 class App{
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -73,6 +78,7 @@ class App{
         this._getPosition();
         form.addEventListener("submit", this._newWorkout.bind(this));
         inputType.addEventListener("change", this._toggleElevationField);
+        containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
     }
 
     _getPosition(){
@@ -87,7 +93,7 @@ class App{
         const {longitude} = position.coords;
         
         const coords = [latitude, longitude];
-        this.#map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
     
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution:
@@ -212,7 +218,25 @@ class App{
             `;
 
             form.insertAdjacentHTML("afterend", html);
-    }
+    };
+
+    _moveToPopup(e){
+        if (!this.#map) return;
+
+        const workoutEl = e.target.closest(".workout");
+
+        if(!workoutEl) return;
+
+        const workout = this.#workouts.find(
+            work => work.id === workoutEl.dataset.id);
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            },
+        });
+    };
 };
 
 const app = new App();

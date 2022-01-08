b92f3e546adb83487486ba7f3c2f110135683ba0
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-01-08 17:37:46 +0300

    General improvements

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Declaring variables
 const modal = document.querySelector(".modal");
@@ -7,28 +7,28 @@ const btnClose = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
 //Declaring functions
-const openModal = function() {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-}
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-const closeModal = function() {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 // Selecting all of the buttons
 for (let i = 0; i < btnsOpen.length; i++) {
-    btnsOpen[i].addEventListener("click", openModal);
+  btnsOpen[i].addEventListener("click", openModal);
 }
 
 // Adding eventlistener to the variables
-btnClose.addEventListener("click", closeModal)
-overlay.addEventListener("click", closeModal)
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
 // Key pressing to close
-document.addEventListener("keydown", function(e){
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-        closeModal()
-    }
-}) 
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
